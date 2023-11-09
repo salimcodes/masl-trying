4af172c813af02dd4e2e26a2415275e21e15c80d
Salim Oyinlola <salimoyinlola@gmail.com>
2023-11-09 20:46:25 +0100

    Added more

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import os
 import openai
-openai.api_type = "azure"
-openai.api_base = "05da51ff63ed47f991ded3a2dccaff23" 
+
 openai.api_key = "05da51ff63ed47f991ded3a2dccaff23"
-openai.api_version = "2023-05-15"
+openai.api_base = "https://tryingopenai.openai.azure.com/" # your endpoint should look like the following https://YOUR_RESOURCE_NAME.openai.azure.com/
+openai.api_type = 'azure'
+openai.api_version = '2023-05-15' # this might change in the future
 
-response = openai.ChatCompletion.create(
-    engine="salimmasl", # engine = "deployment_name".
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
-        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
-        {"role": "user", "content": "Do other Azure AI services support this too?"}
-    ]
-)
+deployment_name='salimqwerty' #This will correspond to the custom name you chose for your deployment when you deployed a model. 
 
-print(response)
-print(response['choices'][0]['message']['content'])
+# Send a completion call to generate an answer
+#print('Sending a test completion job')
+start_phrase = 'Write a tagline for an ice cream shop.'
+response = openai.Completion.create(engine=deployment_name, prompt=start_phrase, max_tokens=10)
+text = response['choices'][0]['text'].replace('\n', '').replace(' .', '.').strip()
+print(start_phrase+text)
